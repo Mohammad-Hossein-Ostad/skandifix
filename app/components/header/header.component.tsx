ffd5d073ba58cd1500cfd8d25aa5f9b769ed7d66
Mohammad-Hossein-Ostad <rajabaliostad@gmail.com>
2026-04-27 21:26:44 +0200

    first commit

---
+++ app/components/header/header.component.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="bg-gray-500">
+      <header>
+        <h1 className="text-5xl">SKANDIFIX</h1>
+      </header>
+    </div>
+  )
+}
